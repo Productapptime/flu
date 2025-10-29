2d1df887117132716efd395010d1e9af18c0d86a
Productapptime <productapptime@gmail.com>
2025-10-29 13:46:18 +0300

    Create pdf_folder_item.dart

---
+++ lib/models/pdf_folder_item.dart
@@ -0,0 +1,28 @@
+// lib/models/pdf_folder_item.dart
+import 'package:flutter/material.dart';
+import 'file_system_item.dart';
+
+class PdfFolderItem extends FileSystemItem {
+  Color color;
+  List<FileSystemItem> items = [];
+  
+  PdfFolderItem({
+    required String id,
+    required String name,
+    this.color = Colors.grey,
+    String? parentFolderId,
+  }) : super(
+    id: id, 
+    name: name, 
+    parentFolderId: parentFolderId,
+  );
+
+  // ✅ Klasördeki dosya sayısı
+  int get fileCount => items.whereType<PdfFileItem>().length;
+  
+  // ✅ Klasördeki klasör sayısı
+  int get folderCount => items.whereType<PdfFolderItem>().length;
+  
+  // ✅ Toplam öğe sayısı
+  int get totalItemCount => items.length;
+}
